62775257e382aa90e42fb1d892dbb572c845e125
Bill McDonough <bill.mcdonough@servicenow.com>
2024-06-20 18:03:02 -0400

    init of ConfigFlow

--- custom_components/fordpass/bmcdonough.py
+++ custom_components/fordpass/bmcdonough.py
@@ -1,25 +1,30 @@
 #!/usr/bin/env python3
+import asyncio
 import logging
 import logging.handlers
 import os
-from dotenv import load_dotenv
 from pdb import set_trace as bp
 
-from const import (
-    CONF_DISTANCE_UNIT,
-    CONF_PRESSURE_UNIT,
-    COORDINATOR,
-    DEFAULT_DISTANCE_UNIT,
-    DEFAULT_PRESSURE_UNIT,
-    DOMAIN,
-    MANUFACTURER,
-    REGION,
-    UPDATE_INTERVAL,
-    UPDATE_INTERVAL_DEFAULT,
-    VEHICLE,
-    VIN,
-)
+from dotenv import load_dotenv
+
+from config_flow import ConfigFlow
 from fordpass_new import Vehicle
+
+# from const import (
+#    CONF_DISTANCE_UNIT,
+#    CONF_PRESSURE_UNIT,
+#    COORDINATOR,
+#    DEFAULT_DISTANCE_UNIT,
+#    DEFAULT_PRESSURE_UNIT,
+#    DOMAIN,
+#    MANUFACTURER,
+#    REGION,
+#    UPDATE_INTERVAL,
+#    UPDATE_INTERVAL_DEFAULT,
+#    VEHICLE,
+#    VIN,
+# )
+
 
 _LOGGER = logging.getLogger(__name__)
 # verbose = True
@@ -29,7 +34,7 @@ debug = True
 
 
 def config_logging():
-    syslog_fmt = "%(filename)s[%(process)d] - [%(name)s:%(lineno)d] :: (%(funcName)s) : %(levelname)s - %(message)s"
+    syslog_fmt = "%(filename)s[%(process)d] - [%(name)s:%(lineno)d] :: (%(funcName)s) %(levelname)s - %(message)s"
     syslog_handler = logging.handlers.SysLogHandler(address="/dev/log", facility="user")
     syslog_handler.setFormatter(logging.Formatter(syslog_fmt))
     _LOGGER.addHandler(syslog_handler)
@@ -58,13 +63,23 @@ def config_logging():
     return None
 
 
-def main():
+async def main():
     try:
         # Check if .env file exists
         if os.path.isfile(".env"):
             # Load environment variables from the .env file
             load_dotenv()
-            bp()
+            # initialize ConfigFlow
+            cf = ConfigFlow()
+            _LOGGER.debug(f"dir(cf)   {dir(cf)}")
+            # initialize async_step_user
+            init_step_user = await cf.async_step_user(None)
+            _LOGGER.debug(f"init_step_user   {init_step_user}")
+            if init_step_user["step_id"] == "user":
+                _LOGGER.debug(f"At step_id: {init_step_user['step_id']}")
+                pass
+            else:
+                _LOGGER.debug("found else")
         return 0  # Indicate successful execution
     except Exception as e:
         _LOGGER.error(f"Error condition: {e}")
@@ -73,8 +88,10 @@ def main():
 
 if __name__ == "__main__":
     config_logging()
-    exit_code = main()
+    exit_code = asyncio.run(main())
     if exit_code == 0:
         _LOGGER.info("Completed successfully.")
+        print("Completed Successfully.")
     else:
         _LOGGER.warning("Failed to complete.")
+        print("Failed to complete.")
